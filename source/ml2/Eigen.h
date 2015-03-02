71f69838feca2bd5cb4da053ce2b79ce5c5ba72b
Ryan Henning <ryan@rhobota.com>
2015-03-02 16:22:47 -0600

    More stuff.

--- source/ml2/Eigen.h
+++ source/ml2/Eigen.h
@@ -1,7 +1,11 @@
-#if NDEBUG
+// #if NDEBUG
+// #include "../ml/Eigen/Core"
+// #else
+// #define NDEBUG 1                   // <-- comment-out these two lines if you need to debug
+// #include "../ml/Eigen/Core"        //     stuff, especially if your program is crashing
+// #undef NDEBUG                      // <-- somewhere inside Eigen doing so will help a lot
+// #endif
+
+
+#define EIGEN_NO_MALLOC 1
 #include "../ml/Eigen/Core"
-#else
-#define NDEBUG 1                   // <-- comment-out these two lines if you need to debug
-#include "../ml/Eigen/Core"        //     stuff, especially if your program is crashing
-#undef NDEBUG                      // <-- somewhere inside Eigen doing so will help a lot
-#endif
