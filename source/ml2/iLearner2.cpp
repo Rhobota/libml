7ef4b5a46b2ff45b5eb50f4bf70ca958fb084809
Ryan Henning <ryan@rhobota.com>
2015-03-03 17:33:17 -0600

    More stuff.

---
+++ source/ml2/iLearner2.cpp
@@ -0,0 +1,21 @@
+#include <ml2/iLearner.h>
+
+
+namespace ml2
+{
+
+
+iLearner* iLearner::newDeserializedLearner(iReadable* readable)
+{
+    // TODO
+    return NULL;
+}
+
+
+void iLearner::serializeLearner(iLearner* learner, iWritable* writable)
+{
+    // TODO
+}
+
+
+}   // namespace ml2
