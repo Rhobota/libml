ec047b08dbdfccd9673e1c0df2ca5eeba632a5be
Ryan Henning <ryan@rhobota.com>
2015-03-04 21:29:25 -0600

    Splitting this out.

---
+++ include/ml2/tLayeredLearnerCPU.h
@@ -0,0 +1,49 @@
+#ifndef __ml2_tLayeredLearnerCPU_h__
+#define __ml2_tLayeredLearnerCPU_h__
+
+
+#include <ml2/tLayeredLearnerBase.h>
+
+
+namespace ml2
+{
+
+
+class tLayeredLearnerCPU : public tLayeredLearnerBase
+{
+    public:
+
+        tLayeredLearnerCPU(u32 numInputDims, u32 numOutputDims);
+
+        tLayeredLearnerCPU(iReadable* in);
+
+        ~tLayeredLearnerCPU();
+
+
+        ///////////////////////////////////////////////////////////////////////
+        // iLearner interface:
+        ///////////////////////////////////////////////////////////////////////
+
+        void update();
+
+        void evaluate(const tIO& input, tIO& output);
+
+        void evaluateBatch(const std::vector<tIO>& inputs,
+                                 std::vector<tIO>& outputs);
+
+        void evaluateBatch(std::vector<tIO>::const_iterator inputStart,
+                           std::vector<tIO>::const_iterator inputEnd,
+                           std::vector<tIO>::iterator outputStart);
+
+        u32 headerId() const;
+
+
+    private:
+
+};
+
+
+}   // namespace ml2
+
+
+#endif   // __ml2_tLayeredLearnerCPU_h__
---
+++ source/ml2/tLayeredLearnerCPU.cpp
@@ -0,0 +1,94 @@
+#include <ml2/tLayeredLearnerCPU.h>
+
+#include <cassert>
+#include <iomanip>
+
+#include "../ml/Eigen.h"
+
+
+namespace ml2
+{
+
+
+tLayeredLearnerCPU::tLayeredLearnerCPU(u32 numInputDims, u32 numOutputDims)
+    : tLayeredLearnerBase(numInputDims, numOutputDims)
+{
+}
+
+tLayeredLearnerCPU::tLayeredLearnerCPU(iReadable* in)
+    : tLayeredLearnerBase(in)
+{
+}
+
+tLayeredLearnerCPU::~tLayeredLearnerCPU()
+{
+    // The super d'tors are called automatically.
+}
+
+void tLayeredLearnerCPU::update()
+{
+    m_update(m_inputMatrix, m_inputMatrixUsed, m_numInputDims,
+             m_targetMatrix, m_targetMatrixUsed, m_numOutputDims);
+    m_clearMatrices();
+}
+
+void tLayeredLearnerCPU::evaluate(const tIO& input, tIO& output)
+{
+    m_clearMatrices();
+    m_copyToInputMatrix(input);
+
+    const fml* outputPtr = NULL;
+    u32 expectedOutputDims = m_numOutputDims;
+    u32 expectedOutputCount = 1;
+
+    m_evaluate(m_inputMatrix, m_inputMatrixUsed, m_numInputDims,
+               outputPtr, expectedOutputDims, expectedOutputCount);
+    m_putOutput(output, outputPtr, expectedOutputDims, expectedOutputCount);
+
+    m_clearMatrices();
+}
+
+void tLayeredLearnerCPU::evaluateBatch(const std::vector<tIO>& inputs,
+                                             std::vector<tIO>& outputs)
+{
+    evaluateBatch(inputs.begin(), inputs.end(), outputs.begin());
+}
+
+void tLayeredLearnerCPU::evaluateBatch(std::vector<tIO>::const_iterator inputStart,
+                                       std::vector<tIO>::const_iterator inputEnd,
+                                       std::vector<tIO>::iterator outputStart)
+{
+    m_clearMatrices();
+    std::vector<tIO>::const_iterator sitr;
+    for (sitr = inputStart; sitr != inputEnd; sitr++)
+        m_copyToInputMatrix(*sitr);
+
+    const fml* outputPtr = NULL;
+    u32 expectedOutputDims = m_numOutputDims;
+    u32 expectedOutputCount = (u32) (inputEnd - inputStart);
+
+    m_evaluate(m_inputMatrix, m_inputMatrixUsed, m_numInputDims,
+               outputPtr, expectedOutputDims, expectedOutputCount);
+    m_putOutput(outputStart, outputPtr, expectedOutputDims, expectedOutputCount);
+
+    m_clearMatrices();
+}
+
+static
+iLearner* s_newLearnerFunc(iReadable* in)
+{
+    return new tLayeredLearnerCPU(in);
+}
+
+static u32 learnerId = 2742490;
+static bool didRegister = iLearner::registerLearnerFuncWithHeaderId(s_newLearnerFunc, learnerId);
+
+u32 tLayeredLearnerCPU::headerId() const
+{
+    if (!didRegister)
+        throw eRuntimeError("Registering my learner id didn't work!");
+    return learnerId;
+}
+
+
+}   // namespace ml2
