7096efa36ca1962933f675a4beaf9ed7e267e930
Ryan Henning <ryan@rhobota.com>
2015-06-12 16:55:17 -0500

    I'm going to make the conv2d functions public now.

---
+++ include/ml/conv2d/cpu_optimized.h
@@ -0,0 +1,44 @@
+#include <ml/common.h>
+
+
+namespace ml
+{
+namespace conv2d
+{
+namespace cpu_optimized
+{
+
+
+void s_conv2d_multi_input(
+        u32 inputCount,  u32 inputStride,  u32 outputStride,
+        const fml* inputPtr,  u32 inputRows,   u32 inputCols,   u32 inputComponents,
+        const fml* kernelPtr, u32 kernelRows,  u32 kernelCols,
+                              u32 kernelStepY, u32 kernelStepX,
+                              u32 numKernels,
+        const fml* kernelBiases, fml scaleFactor,
+              fml* outputPtr);
+
+
+void s_conv2d_backprop_multi_input(
+        u32 inputCount,  u32 inputStride,  u32 outputStride,
+              fml* di_ptr,    u32 inputRows,   u32 inputCols,   u32 inputComponents,
+        const fml* kernelPtr, u32 kernelRows,  u32 kernelCols,
+                              u32 kernelStepY, u32 kernelStepX,
+                              u32 numKernels,
+        const fml* kernelBiases, fml scaleFactor,
+        const fml* dA_ptr)
+
+
+void s_conv2d_accumError_multi_input(
+        u32 inputCount,  u32 inputStride,  u32 outputStride,
+        const fml* inputPtr, u32 inputRows,   u32 inputCols,   u32 inputComponents,
+              fml* dk_ptr,   u32 kernelRows,  u32 kernelCols,
+                             u32 kernelStepY, u32 kernelStepX,
+                             u32 numKernels,
+              fml* db_ptr, fml scaleFactor,
+        const fml* dA_ptr);
+
+
+}  // namespace cpu_optimized
+}  // namespace conv2d
+}  // namespace ml
