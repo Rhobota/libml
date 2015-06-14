be2bca7e5e361f9e6dc55df7f7ebcdce5e6fff0c
Ryan Henning <ryan@rhobota.com>
2015-06-13 19:45:54 -0500

    Moved the testing helper functions into the gpu.h and gpu.cu files.

--- include/ml/conv2d/gpu.h
+++ include/ml/conv2d/gpu.h
@@ -39,6 +39,44 @@ void conv2d_accumError_multi_input(
         const fml* dA_ptr);
 
 
+/*
+ * The following functions are just helpers to test this GPU implementation.
+ * They assume you're passing in host buffers, so these functions will allocate
+ * device buffers and will copy the input to them. Also, these functions will
+ * copy the output back into the host buffers. Don't use these in real production
+ * code because obviously all that copying is not efficient! Use the three functions
+ * ABOVE instead because those assume you're passing device pointers so no copying
+ * is needed.
+ */
+
+void conv2d_multi_input_with_memcpy(
+        u32 inputCount,  u32 inputStride,  u32 outputStride,
+        const fml* inputPtr,  u32 inputRows,   u32 inputCols,   u32 inputComponents,
+        const fml* kernelPtr, u32 kernelRows,  u32 kernelCols,
+                              u32 kernelStepY, u32 kernelStepX,
+                              u32 numKernels,
+        const fml* kernelBiases, fml scaleFactor,
+              fml* outputPtr);
+
+void conv2d_backprop_multi_input_with_memcpy(
+        u32 inputCount,  u32 inputStride,  u32 outputStride,
+              fml* di_ptr,    u32 inputRows,   u32 inputCols,   u32 inputComponents,
+        const fml* kernelPtr, u32 kernelRows,  u32 kernelCols,
+                              u32 kernelStepY, u32 kernelStepX,
+                              u32 numKernels,
+        const fml* kernelBiases, fml scaleFactor,
+        const fml* dA_ptr);
+
+void conv2d_accumError_multi_input_with_memcpy(
+        u32 inputCount,  u32 inputStride,  u32 outputStride,
+        const fml* inputPtr, u32 inputRows,   u32 inputCols,   u32 inputComponents,
+              fml* dk_ptr,   u32 kernelRows,  u32 kernelCols,
+                             u32 kernelStepY, u32 kernelStepX,
+                             u32 numKernels,
+              fml* db_ptr, fml scaleFactor,
+        const fml* dA_ptr);
+
+
 }  // namespace gpu
 }  // namespace conv2d
 }  // namespace ml
--- tests/ml/conv2d_gpu_test.cpp
+++ tests/ml/conv2d_gpu_test.cpp
@@ -15,154 +15,6 @@ using ml::fml;
 static const int kTestIterations = 3000;
 
 
-#define cuda_assert(expression) \
-    do { \
-        cudaError_t err; \
-        if ((err = (expression)) != cudaSuccess) \
-        { \
-            std::cout << "Cuda error: " << cudaGetErrorString(err) << std::endl; \
-            assert(false); \
-        } \
-    } while (false)
-
-
-#define cublas_assert(expression, what) \
-    do { \
-        if ((expression) != CUBLAS_STATUS_SUCCESS) \
-        { \
-            std::cout << "cuBLAS error! " << what << std::endl; \
-            assert(false); \
-        } \
-    } while (false)
-
-
-static
-fml* s_cudaMalloc(u32 size)
-{
-    fml* ptr = NULL;
-    cuda_assert( cudaMalloc((void**)(&ptr), size*sizeof(fml)) );
-    return ptr;
-}
-
-
-static
-void s_cudaFree(fml*& buf)
-{
-    if (buf)
-    {
-        cuda_assert( cudaFree(buf) );
-        buf = NULL;
-    }
-}
-
-
-static
-void s_cudaCopyHostToDevice(fml* dest, const fml* source, u32 size)
-{
-    cuda_assert( cudaMemcpy(dest, source, size*sizeof(fml), cudaMemcpyHostToDevice) );
-}
-
-
-static
-void s_cudaCopyDeviceToHost(fml* dest, const fml* source, u32 size)
-{
-    cuda_assert( cudaMemcpy(dest, source, size*sizeof(fml), cudaMemcpyDeviceToHost) );
-}
-
-
-void gpu_helper_conv2d_multi_input(
-        u32 inputCount,  u32 inputStride,  u32 outputStride,
-        const fml* inputPtr,  u32 inputRows,   u32 inputCols,   u32 inputComponents,
-        const fml* kernelPtr, u32 kernelRows,  u32 kernelCols,
-                              u32 kernelStepY, u32 kernelStepX,
-                              u32 numKernels,
-        const fml* kernelBiases, fml scaleFactor,
-              fml* outputPtr)
-{
-    fml* inputPtr_gpu = s_cudaMalloc(inputCount * inputStride);
-    fml* kernelPtr_gpu = s_cudaMalloc(kernelRows * kernelCols * inputComponents * numKernels);
-    fml* kernelBiases_gpu = s_cudaMalloc(numKernels);
-    fml* outputPtr_gpu = s_cudaMalloc(inputCount * outputStride);
-
-    ml::conv2d::gpu::conv2d_multi_input(
-        inputCount,  inputStride,  outputStride,
-        inputPtr_gpu,  inputRows,   inputCols,   inputComponents,
-        kernelPtr_gpu, kernelRows,  kernelCols,
-                              kernelStepY, kernelStepX,
-                              numKernels,
-        kernelBiases_gpu, fml scaleFactor,
-        outputPtr_gpu
-    );
-
-    s_cudaFree(inputPtr_gpu);
-    s_cudaFree(kernelPtr_gpu);
-    s_cudaFree(kernelBiases_gpu);
-    s_cudaFree(outputPtr_gpu);
-}
-
-
-void gpu_helper_conv2d_backprop_multi_input(
-        u32 inputCount,  u32 inputStride,  u32 outputStride,
-              fml* di_ptr,    u32 inputRows,   u32 inputCols,   u32 inputComponents,
-        const fml* kernelPtr, u32 kernelRows,  u32 kernelCols,
-                              u32 kernelStepY, u32 kernelStepX,
-                              u32 numKernels,
-        const fml* kernelBiases, fml scaleFactor,
-        const fml* dA_ptr)
-{
-    fml* di_ptr_gpu = s_cudaMalloc(inputCount * inputStride);
-    fml* kernelPtr_gpu = s_cudaMalloc(kernelRows * kernelCols * inputComponents * numKernels);
-    fml* kernelBiases_gpu = s_cudaMalloc(numKernels);
-    fml* dA_ptr_gpu = s_cudaMalloc(inputCount * outputStride);
-
-    ml::conv2d::gpu::conv2d_backprop_multi_input(
-        inputCount,  inputStride,  outputStride,
-        di_ptr_gpu,    inputRows,   inputCols,   inputComponents,
-        kernelPtr_gpu, kernelRows,  kernelCols,
-                              kernelStepY, kernelStepX,
-                              numKernels,
-        kernelBiases_gpu, scaleFactor,
-        dA_ptr_gpu
-    );
-
-    s_cudaFree(di_ptr_gpu);
-    s_cudaFree(kernelPtr_gpu);
-    s_cudaFree(kernelBiases_gpu);
-    s_cudaFree(dA_ptr_gpu);
-}
-
-
-void gpu_helper_conv2d_accumError_multi_input(
-        u32 inputCount,  u32 inputStride,  u32 outputStride,
-        const fml* inputPtr, u32 inputRows,   u32 inputCols,   u32 inputComponents,
-              fml* dk_ptr,   u32 kernelRows,  u32 kernelCols,
-                             u32 kernelStepY, u32 kernelStepX,
-                             u32 numKernels,
-              fml* db_ptr, fml scaleFactor,
-        const fml* dA_ptr)
-{
-    fml* inputPtr_gpu = s_cudaMalloc(inputCount * inputStride);
-    fml* dk_ptr_gpu = s_cudaMalloc(kernelRows * kernelCols * inputComponents * numKernels);
-    fml* db_ptr_gpu = s_cudaMalloc(numKernels);
-    fml* dA_ptr_gpu = s_cudaMalloc(inputCount * outputStride);
-
-    ml::conv2d::gpu::conv2d_accumError_multi_input(
-        inputCount,  inputStride,  outputStride,
-        inputPtr_gpu, inputRows,   inputCols,   inputComponents,
-        dk_ptr_gpu,   kernelRows,  kernelCols,
-                             kernelStepY, kernelStepX,
-                             numKernels,
-        db_ptr_gpu, scaleFactor,
-        dA_ptr_gpu
-    );
-
-    s_cudaFree(inputPtr_gpu);
-    s_cudaFree(dk_ptr_gpu);
-    s_cudaFree(db_ptr_gpu);
-    s_cudaFree(dA_ptr_gpu);
-}
-
-
 void convolveTest(
         const tTest& t,
 
@@ -201,7 +53,7 @@ void convolveTest(
     for (u32 i = 0; i < numKernels; i++)
         kernelBiases[i] = rand() % 100;
 
-    gpu_helper_conv2d_multi_input(
+    ml::conv2d::gpu::conv2d_multi_input_with_memcpy(
             numInputs, inputRows*inputCols*inputComponents, outputRows*outputCols*numKernels,
             input, inputRows, inputCols, inputComponents,
             kernels, kernelRows, kernelCols,
@@ -349,7 +201,7 @@ void backpropTest(
     for (u32 i = 0; i < numKernels; i++)
         kernelBiases[i] = rand() % 100;
 
-    gpu_helper_conv2d_backprop_multi_input(
+    ml::conv2d::gpu::conv2d_backprop_multi_input_with_memcpy(
             numInputs, inputRows*inputCols*inputComponents, outputRows*outputCols*numKernels,
             di1, inputRows, inputCols, inputComponents,
             kernels, kernelRows, kernelCols,
@@ -499,7 +351,7 @@ void accumErrorTest(
     for (u32 i = 0; i < numKernels; i++)
         db2[i] = FML(400000.0);
 
-    gpu_helper_conv2d_accumError_multi_input(
+    ml::conv2d::gpu::conv2d_accumError_multi_input_with_memcpy(
             numInputs, inputRows*inputCols*inputComponents, outputRows*outputCols*numKernels,
             input, inputRows, inputCols, inputComponents,
             dk1, kernelRows, kernelCols,
