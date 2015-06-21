9bb6612b2cb0dcbf7e8813b15f31bc630cd8b280
Ryan Henning <ryan@rhobota.com>
2015-06-20 22:40:16 -0500

    Reorg.

--- source/ml/conv2d/gpu_part1.cu
+++ source/ml/conv2d/gpu_part1.cu
@@ -1,32 +1,6 @@
 #include <ml/conv2d/gpu.h>
 
-#include "../cuda_stuff.ipp"
-
-#define GPU_PART1_USE_TEMPLATE 0
-#include "gpu_part1.ipp"
-#undef  GPU_PART1_USE_TEMPLATE
-
-#define GPU_PART1_USE_TEMPLATE 1
-#include "gpu_part1.ipp"
-#undef  GPU_PART1_USE_TEMPLATE
-
-
-/*
- * If defined, this file will take a long time to compile, but
- * will output very fast code. You should turn this on for
- * production code, but turn it off to do quick test iterations.
- */
-#define COMPILE_A_BUNCH_OF_TEMPLATES_TO_MAKE_FAST_CODE 1
-
-
-/*
- * If defined, this code will refuse to run the fallback
- * implementation, which is a very slow implementation.
- * This is nice to turn on if you want to be sure your
- * code uses the fast templated versions of the function
- * instead of the fallback.
- */
-#define THROW_IF_FALLBACK_IMPL_NEEDED 0
+#include "gpu_conv.ipp"
 
 
 namespace ml
@@ -35,103 +9,6 @@ namespace conv2d
 {
 namespace gpu
 {
-
-
-#if THROW_IF_FALLBACK_IMPL_NEEDED
-#define RUN_FALLBACK_IMPL \
-    throw eRuntimeError("The fallback (aka, slow) implementation is needed to convolve this input. But we've turned off the fallback implementation, so you'll need to turn it on or (preferably) modify your code to use one of the fast implementation paths.");
-#else
-#define RUN_FALLBACK_IMPL \
-    if (canUseFastImpl) \
-    { \
-        gpu_conv2d_multi_input<<<gridSize, blockSize, sharedMemNeeded>>>( \
-            inputComponents, kernelRows, kernelCols, kernelStepY, kernelStepX, numKernels, \
-            inputPtr,  inputRows,   inputCols, \
-            kernelPtr, \
-            kernelBiases, scaleFactor, \
-            outputPtr, outputRows, outputCols); \
-    } \
-    else \
-    { \
-        gpu_conv2d_multi_input_for_large_input<<<gridSize, blockSize, sharedMemNeeded>>>( \
-            inputComponents, kernelRows, kernelCols, kernelStepY, kernelStepX, numKernels, \
-            inputPtr,  inputRows,   inputCols, \
-            kernelPtr, \
-            kernelBiases, scaleFactor, \
-            outputPtr, outputRows, outputCols); \
-    }
-#endif
-
-
-#define SWITCH_KERNEL_DIMS(inputComponents, kernelStepY, kernelStepX, numKernels) \
-    switch ((kernelRows * 0x10) + kernelCols) \
-    { \
-        case 0x33: \
-        { \
-            if (canUseFastImpl) \
-            { \
-                gpu_conv2d_multi_input_templated<inputComponents, 3, 3, kernelStepY, kernelStepX, numKernels><<<gridSize, blockSize, sharedMemNeeded>>>( \
-                    inputPtr,  inputRows,   inputCols, \
-                    kernelPtr, \
-                    kernelBiases, scaleFactor, \
-                    outputPtr, outputRows, outputCols); \
-            } \
-            else \
-            { \
-                gpu_conv2d_multi_input_for_large_input_templated<inputComponents, 3, 3, kernelStepY, kernelStepX, numKernels><<<gridSize, blockSize, sharedMemNeeded>>>( \
-                    inputPtr,  inputRows,   inputCols, \
-                    kernelPtr, \
-                    kernelBiases, scaleFactor, \
-                    outputPtr, outputRows, outputCols); \
-            } \
-            break; \
-        } \
-        case 0x55: \
-        { \
-            if (canUseFastImpl) \
-            { \
-                gpu_conv2d_multi_input_templated<inputComponents, 5, 5, kernelStepY, kernelStepX, numKernels><<<gridSize, blockSize, sharedMemNeeded>>>( \
-                    inputPtr,  inputRows,   inputCols, \
-                    kernelPtr, \
-                    kernelBiases, scaleFactor, \
-                    outputPtr, outputRows, outputCols); \
-            } \
-            else \
-            { \
-                gpu_conv2d_multi_input_for_large_input_templated<inputComponents, 5, 5, kernelStepY, kernelStepX, numKernels><<<gridSize, blockSize, sharedMemNeeded>>>( \
-                    inputPtr,  inputRows,   inputCols, \
-                    kernelPtr, \
-                    kernelBiases, scaleFactor, \
-                    outputPtr, outputRows, outputCols); \
-            } \
-            break; \
-        } \
-        default: \
-            RUN_FALLBACK_IMPL \
-    } \
-
-
-#define SWITCH_KERNEL_STEP(inputComponents, numKernels) \
-    switch ((kernelStepY * 0x10) + kernelStepX) \
-    { \
-        case 0x11: SWITCH_KERNEL_DIMS(inputComponents, 1, 1, numKernels)  break; \
-        case 0x22: SWITCH_KERNEL_DIMS(inputComponents, 2, 2, numKernels)  break; \
-        default: \
-            RUN_FALLBACK_IMPL \
-    } \
-
-
-#define SWITCH_NUM_KERNELS(inputComponents) \
-    switch (numKernels) \
-    { \
-        case 4: SWITCH_KERNEL_STEP(inputComponents, 4)  break; \
-        case 8: SWITCH_KERNEL_STEP(inputComponents, 8)  break; \
-        case 16: SWITCH_KERNEL_STEP(inputComponents, 16)  break; \
-        case 32: SWITCH_KERNEL_STEP(inputComponents, 32)  break; \
-        case 64: SWITCH_KERNEL_STEP(inputComponents, 64)  break; \
-        default: \
-            RUN_FALLBACK_IMPL \
-    } \
 
 
 void conv2d_multi_input(
@@ -191,26 +68,7 @@ void conv2d_multi_input(
     u32 outputRows = (inputRows - 1) / kernelStepY + 1;
     u32 outputCols = (inputCols - 1) / kernelStepX + 1;
 
-#if COMPILE_A_BUNCH_OF_TEMPLATES_TO_MAKE_FAST_CODE
-    switch (inputComponents)
-    {
-        case 1: SWITCH_NUM_KERNELS(1)  break;
-        case 2: SWITCH_NUM_KERNELS(2)  break;
-        case 3: SWITCH_NUM_KERNELS(3)  break;
-        case 4: SWITCH_NUM_KERNELS(4)  break;
-        case 5: SWITCH_NUM_KERNELS(5)  break;
-        case 6: SWITCH_NUM_KERNELS(6)  break;
-        case 7: SWITCH_NUM_KERNELS(7)  break;
-        case 8: SWITCH_NUM_KERNELS(8)  break;
-        case 16: SWITCH_NUM_KERNELS(16)  break;
-        case 32: SWITCH_NUM_KERNELS(32)  break;
-        case 64: SWITCH_NUM_KERNELS(64)  break;
-        default:
-            RUN_FALLBACK_IMPL
-    }
-#else
-    RUN_FALLBACK_IMPL
-#endif
+    RUN_CONV2D_GPU_FUNTION
 
     cudaError_t errSync  = cudaGetLastError();
     if (errSync != cudaSuccess)
