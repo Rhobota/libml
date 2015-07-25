2e9ac1dda28740472de694ceb12f361fa2f6a5e2
Ryan Henning <ryan@rhobota.com>
2015-07-25 13:51:05 -0500

    Finally getting back to this... and now I'm starting the accum error CUDA function.

--- source/ml/conv2d/gpu_accum.cu
+++ source/ml/conv2d/gpu_accum.cu
@@ -1,6 +1,6 @@
 #include <ml/conv2d/gpu.h>
 
-//#include "gpu_part3.ipp"
+#include "gpu_accum.ipp"
 
 
 namespace ml
@@ -22,6 +22,57 @@ void conv2d_accumError_multi_input(
 {
     // TODO
     // Don't forget to set dk_ptr and db_ptr vectors to zero before you begin.
+
+    assert(inputPtr && inputRows > 0 && inputCols > 0 && inputComponents > 0);
+    assert(dk_ptr && (kernelRows % 2) == 1 && (kernelCols % 2) == 1);
+    assert(kernelStepY > 0 && kernelStepX > 0 && numKernels > 0);
+    assert(db_ptr);
+    assert(dA_ptr);
+
+    if (kernelRows != 1 && kernelRows != 3 && kernelRows != 5 && kernelRows != 7)
+        throw eInvalidArgument("Unsupported kernelRows: must be 1, 3, 5, or 7.");
+    if (kernelCols != 1 && kernelCols != 3 && kernelCols != 5 && kernelCols != 7)
+        throw eInvalidArgument("Unsupported kernelCols: must be 1, 3, 5, or 7.");
+
+    if (kernelStepY != 1)
+        throw eInvalidArgument("Unsupported kernelStepY: must be in 1.");
+    if (kernelStepX != 1)
+        throw eInvalidArgument("Unsupported kernelStepX: must be in 1.");
+
+    if (numKernels > MAX_KERNELS_SUPPORTED)
+        throw eInvalidArgument("Unsupported numKernels: you specified too many!");
+
+    u32 kernelRadiusY = kernelRows / 2;
+    u32 kernelRadiusX = kernelCols / 2;
+
+    u32 effectiveBlockSizeY = BLOCK_SIZE_Y - 2*kernelRadiusY;  // Each block of threads will fill
+    u32 effectiveBlockSizeX = BLOCK_SIZE_X - 2*kernelRadiusX;  // a smaller block of output, because we need
+                                                               // an "apron" so that our kernel doesn't fall off
+                                                               // the side and into no-where-land.
+
+    dim3 gridSize;
+    gridSize.x = (inputCols-1) / effectiveBlockSizeX + 1;
+    gridSize.y = (inputRows-1) / effectiveBlockSizeY + 1;
+    gridSize.z = inputCount;
+
+    dim3 blockSize;
+    blockSize.x = BLOCK_SIZE_X;
+    blockSize.y = BLOCK_SIZE_Y;
+    blockSize.z = 1;
+
+    bool canUseFastImpl = true;
+    u32 sharedMemNeeded = (BLOCK_SIZE_Y*BLOCK_SIZE_X*inputComponents + kernelRows*kernelCols*inputComponents*numKernels + numKernels) * sizeof(fml);
+    if (sharedMemNeeded * DESIRED_BLOCKS_PER_SM > SHARED_MEM_AVAIL_PER_SM)
+    {
+        canUseFastImpl = false;
+        sharedMemNeeded = (BLOCK_SIZE_Y*BLOCK_SIZE_X + kernelRows*kernelCols*inputComponents*numKernels + numKernels) * sizeof(fml);
+    }
+
+    RUN_CONV2D_ACCUM_GPU_FUNTION
+
+    cudaError_t errSync  = cudaGetLastError();
+    if (errSync != cudaSuccess)
+        throw eRuntimeError(std::string("CUDA launch error: ") + cudaGetErrorString(errSync));
 }
 
 
