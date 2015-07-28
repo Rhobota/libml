5ef6207d9d6c06ea5264c9fb3962daf9ebfbc87b
Ryan Henning <ryan@rhobota.com>
2015-07-28 18:25:43 -0500

    Unpool impl.

--- source/ml/pool2d/cpu_golden.cpp
+++ source/ml/pool2d/cpu_golden.cpp
@@ -42,6 +42,44 @@ void s_pool2d(
 }
 
 
+static
+void s_un_pool2d(
+        const fml* inputPtr,  u32 inputRows,  u32 inputCols,  u32 inputComponents,
+                              u32 poolRows,  u32 poolCols,
+        const fml* srcPtr,
+              fml* destPtr)
+{
+    assert(inputPtr);
+    assert(inputRows > 0 && inputCols > 0 && inputComponents > 0);
+    assert(poolRows > 0 && poolCols > 0);
+    assert(srcPtr);
+    assert(destPtr);
+
+    u32 outRows = inputRows / poolRows;
+    u32 outCols = inputCols / poolCols;
+
+    for (u32 r = 0; r < outRows; r++)
+    {
+        for (u32 c = 0; c < outCols; c++)
+        {
+            for (u32 i = 0; i < inputComponents; i++)
+            {
+                MapRowMajorWithStrideConst input(inputPtr+i,
+                                                 inputRows, inputCols,
+                                                 Stride(inputCols*inputComponents, inputComponents));
+                MapRowMajorWithStrideConst::Index row = 0, col = 0;
+                input.block(r*poolRows, c*poolCols, poolRows, poolCols).maxCoeff(&row, &col);
+                fml value = *srcPtr++;
+                MapRowMajorWithStride dest(destPtr+i,
+                                           inputRows, inputCols,
+                                           Stride(inputCols*inputComponents, inputComponents));
+                dest(r*poolRows + row, c*poolCols + col) = value;
+            }
+        }
+    }
+}
+
+
 void pool2d_multi_input(
         u32 inputCount,
         const fml* inputPtr,  u32 inputRows,  u32 inputCols,  u32 inputComponents,
@@ -66,7 +104,15 @@ void un_pool2d_multi_input(
         const fml* srcPtr,
               fml* destPtr)
 {
-    // TODO
+    u32 inputStride = inputRows * inputCols * inputComponents;
+    u32 outputStride = (inputRows / poolRows) * (inputCols / poolCols) * inputComponents;
+    for (u32 i = 0; i < inputCount; i++)
+    {
+        s_un_pool2d(inputPtr + i*inputStride, inputRows, inputCols, inputComponents,
+                                              poolRows, poolCols,
+                    srcPtr + i*outputStride,
+                    destPtr + i*inputStride);
+    }
 }
 
 
