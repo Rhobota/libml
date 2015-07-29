821d8d35139e8c67f24f1b4804329efa2b577e66
Ryan Henning <ryan@rhobota.com>
2015-07-28 21:55:28 -0500

    Cont'.

--- source/ml/pool2d/pool2d_gpu.cu
+++ source/ml/pool2d/pool2d_gpu.cu
@@ -1,5 +1,7 @@
 #include <ml/pool2d/gpu.h>
 
+#include "../cuda_stuff.ipp"
+
 
 namespace ml
 {
@@ -36,7 +38,26 @@ void pool2d_multi_input_with_memcpy(
                               u32 poolRows,  u32 poolCols,
               fml* outputPtr)
 {
-    // TODO
+    u32 outputRows = inputRows / poolRows;
+    u32 outputCols = inputCols / poolCols;
+
+    fml* inputPtr_gpu = s_cudaMalloc(inputRows*inputCols*inputComponents*inputCount);
+    fml* outputPtr_gpu = s_cudaMalloc(outputRows*outputCols*inputComponents*inputCount);
+
+    s_cudaCopyHostToDevice(inputPtr_gpu, inputPtr, inputRows*inputCols*inputComponents*inputCount);
+    s_cudaCopyHostToDevice(outputPtr_gpu, outputPtr, outputRows*outputCols*inputComponents*inputCount);
+
+    pool2d_multi_input(
+        inputCount,
+        inputPtr_gpu,  inputRows,   inputCols,   inputComponents,
+                       poolRows,  poolCols,
+        outputPtr_gpu
+    );
+
+    s_cudaCopyDeviceToHost(outputPtr, outputPtr_gpu, outputRows*outputCols*inputComponents*inputCount);
+
+    s_cudaFree(inputPtr_gpu);
+    s_cudaFree(outputPtr_gpu);
 }
 
 
