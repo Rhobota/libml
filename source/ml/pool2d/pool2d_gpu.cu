41225a60aa593b68c1d191857b6362a02bc8c32d
Ryan Henning <ryan@rhobota.com>
2015-07-29 00:45:45 -0500

    Cont'.

--- source/ml/pool2d/pool2d_gpu.cu
+++ source/ml/pool2d/pool2d_gpu.cu
@@ -56,8 +56,8 @@ void pool2d_multi_input_with_memcpy(
 
     s_cudaCopyDeviceToHost(outputPtr, outputPtr_gpu, outputRows*outputCols*inputComponents*inputCount);
 
-    s_cudaFree(inputPtr_gpu);
     s_cudaFree(outputPtr_gpu);
+    s_cudaFree(inputPtr_gpu);
 }
 
 
@@ -68,7 +68,30 @@ void un_pool2d_multi_input_with_memcpy(
         const fml* srcPtr,
               fml* destPtr)
 {
-    // TODO
+    u32 outputRows = inputRows / poolRows;
+    u32 outputCols = inputCols / poolCols;
+
+    fml* inputPtr_gpu = s_cudaMalloc(inputRows*inputCols*inputComponents*inputCount);
+    fml* srcPtr_gpu = s_cudaMalloc(outputRows*outputCols*inputComponents*inputCount);
+    fml* destPtr_gpu = s_cudaMalloc(inputRows*inputCols*inputComponents*inputCount);
+
+    s_cudaCopyHostToDevice(inputPtr_gpu, inputPtr, inputRows*inputCols*inputComponents*inputCount);
+    s_cudaCopyHostToDevice(srcPtr_gpu, srcPtr, outputRows*outputCols*inputComponents*inputCount);
+    s_cudaCopyHostToDevice(destPtr_gpu, destPtr, inputRows*inputCols*inputComponents*inputCount);
+
+    un_pool2d_multi_input(
+        inputCount,
+        inputPtr_gpu,  inputRows,   inputCols,   inputComponents,
+                       poolRows,  poolCols,
+        srcPtr_gpu,
+        destPtr_gpu
+    );
+
+    s_cudaCopyDeviceToHost(destPtr, destPtr_gpu, inputRows*inputCols*inputComponents*inputCount);
+
+    s_cudaFree(destPtr_gpu);
+    s_cudaFree(srcPtr_gpu);
+    s_cudaFree(inputPtr_gpu);
 }
 
 
