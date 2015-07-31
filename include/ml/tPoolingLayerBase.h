ddf5fa2340372cc4ca74b4e6ce857c76b3712bb9
Ryan Henning <ryan@rhobota.com>
2015-07-30 22:07:07 -0500

    The next thing to implement!

---
+++ include/ml/tPoolingLayerBase.h
@@ -0,0 +1,71 @@
+#ifndef __ml_tPoolingLayerBase_h__
+#define __ml_tPoolingLayerBase_h__
+
+
+#include <ml/common.h>
+
+
+namespace ml
+{
+
+
+class tPoolingLayerBase : public iLayer, public bNonCopyable
+{
+    public:
+
+        /**
+         * Constructs a pooling layer. Yay! Right now only MAX-pooling is
+         * implemented. I'd recommend doing 2x2 pooling, because that already
+         * cuts down the dimensionality by a factor of 4, which is quite a lot
+         * really. You probably don't need any more reduction than that from
+         * one layer, especially a simple layer like this one.
+         */
+        tPoolingLayerBase(u32 poolRows = 2, u32 poolCols = 2);
+
+        /**
+         * D'tor.
+         */
+        ~tPoolingLayerBase();
+
+
+        ///////////////////////////////////////////////////////////////////////
+        // The iLayer interface:   (partial interface only)
+        ///////////////////////////////////////////////////////////////////////
+
+        fml calculateError(const tIO& output, const tIO& target);
+
+        fml calculateError(const std::vector<tIO>& outputs,
+                           const std::vector<tIO>& targets);
+
+        void reset();
+
+        void printLayerInfo(std::ostream& out) const;
+
+        std::string layerInfoString() const;
+
+
+        ///////////////////////////////////////////////////////////////////////
+        // The iPackable interface:
+        ///////////////////////////////////////////////////////////////////////
+
+        void pack(iWritable* out) const;
+        void unpack(iReadable* in);
+
+
+    protected:
+
+        u32 m_inputRows;
+        u32 m_inputCols;
+
+        u32 m_poolRows;
+        u32 m_poolCols;
+
+        u32 m_curCount;
+        u32 m_maxCount;
+};
+
+
+}   // namespace ml
+
+
+#endif   // __ml_tPoolingLayerBase_h__
---
+++ include/ml/tPoolingLayerGPU.h
@@ -0,0 +1,55 @@
+#ifndef __ml_tPoolingLayerGPU_h__
+#define __ml_tPoolingLayerGPU_h__
+
+
+#include <ml/tPoolingLayerBase.h>
+
+
+namespace ml
+{
+
+
+class tPoolingLayerGPU : public tPoolingLayerBase
+{
+    public:
+
+        /**
+         * See tPoolingLayerBase::tPoolingLayerBase().
+         */
+        tPoolingLayerGPU();
+
+        /**
+         * D'tor.
+         */
+        ~tPoolingLayerGPU();
+
+
+        ///////////////////////////////////////////////////////////////////////
+        // The iLayer interface:   (partial interface only)
+        ///////////////////////////////////////////////////////////////////////
+
+        void takeInput(const fml* input, u32 numInputDims, u32 count);
+
+        const fml* getOutput(u32& numOutputDims, u32& count) const;
+
+        void takeOutputErrorGradients(
+                          const fml* outputErrorGradients, u32 numOutputDims, u32 outputCount,
+                          const fml* input, u32 numInputDims, u32 inputCount,
+                          bool calculateInputErrorGradients);
+
+        const fml* getInputErrorGradients(u32& numInputDims, u32& count) const;
+
+        u32 headerId() const;
+
+
+    private:
+
+        fml* m_gpu_a;
+        fml* m_gpu_prev_da;
+};
+
+
+}   // namespace ml
+
+
+#endif   // __ml_tPoolingLayerGPU_h__
