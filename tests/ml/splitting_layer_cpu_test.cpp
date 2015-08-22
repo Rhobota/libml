5a785abe432f401d17d1b741283dcffce5303d8f
Ryan Henning <ryan@rhobota.com>
2015-08-22 14:42:35 -0500

    This test is finished.

--- tests/ml/splitting_layer_cpu_test.cpp
+++ tests/ml/splitting_layer_cpu_test.cpp
@@ -1,5 +1,6 @@
 #include <ml/tSplittingLayerCPU.h>
 #include <ml/tScalingLayerCPU.h>
+#include <ml/tReductionLayerCPU.h>
 
 #include <rho/tTest.h>
 #include <rho/tCrashReporter.h>
@@ -15,7 +16,7 @@ static const int kTestIterations = 100;
 static const int kTestInnerIterations = 100;
 
 
-void test(const tTest& t)
+void test1(const tTest& t)
 {
     u32 numInputDims = (rand() % 20) + 1;
     u32 numOutputDims = numInputDims;
@@ -135,13 +136,142 @@ void test(const tTest& t)
 }
 
 
+void test2(const tTest& t)
+{
+    u32 numInputDims = (rand() % 20) + 1;
+
+    std::vector<u32> layerInfo;
+
+    u32 sum = 0;
+    while (sum < numInputDims)
+    {
+        u32 innerInputDims = (rand() % (numInputDims - sum)) + 1;
+        sum += innerInputDims;
+        layerInfo.push_back(innerInputDims);
+    }
+    t.assert(sum == numInputDims);
+
+    u32 numOutputDims = layerInfo.size();
+    ml::tSplittingLayerCPU layer(numInputDims, numOutputDims);
+
+    for (size_t i = 0; i < layerInfo.size(); i++)
+    {
+        u32 innerInputDims = layerInfo[i];
+        u32 innerOutputDims = 1;
+        ml::iLayer* innerLayer = new ml::tReductionLayerCPU(innerInputDims, innerOutputDims);
+        layer.addLayer(innerLayer, innerInputDims, innerOutputDims);
+    }
+
+    for (int iter = 0; iter < kTestInnerIterations; iter++)
+    {
+        u32 count = (rand() % 10) + 1;
+        fml* input          = new fml[numInputDims * count];
+        fml* correctOutput  = new fml[numOutputDims * count];
+        fml* outError       = new fml[numOutputDims * count];
+        fml* correctInError = new fml[numInputDims * count];
+
+        // Forward pass.
+        {
+            for (u32 c = 0; c < count; c++)
+                for (u32 i = 0; i < numInputDims; i++)
+                    input[c*numInputDims + i] = (rand() % 100) * (c + 1);
+
+            for (u32 c = 0; c < count; c++)
+            {
+                u32 index = 0;
+                for (size_t i = 0; i < layerInfo.size(); i++)
+                {
+                    fml sum = FML(0.0);
+                    for (u32 o = 0; o < layerInfo[i]; o++)
+                    {
+                        sum += input[c*numInputDims + index];
+                        index++;
+                    }
+                    correctOutput[c*numOutputDims + i] = sum;
+                }
+                t.assert(index == numInputDims);
+                t.assert(layerInfo.size() == numOutputDims);
+            }
+
+            layer.takeInput(input, numInputDims, count);
+
+            u32 retNumOutputDims = 0, retCount = 0;
+            const fml* output = layer.getOutput(retNumOutputDims, retCount);
+            t.assert(retNumOutputDims == numOutputDims);
+            t.assert(retCount == count);
+
+            for (u32 c = 0; c < count; c++)
+            {
+                for (u32 o = 0; o < numOutputDims; o++)
+                {
+                    if (fabs(correctOutput[c*numOutputDims + o] - output[c*numOutputDims + o]) > 0.00001)
+                    {
+                        std::cerr << correctOutput[c*numOutputDims + o] << "  " << output[c*numOutputDims + o] << std::endl;
+                        t.fail();
+                    }
+                }
+            }
+        }
+
+        // Backward pass.
+        {
+            for (u32 c = 0; c < count; c++)
+                for (u32 o = 0; o < numOutputDims; o++)
+                    outError[c*numOutputDims + o] = (rand() % 100) * (c + 1);
+
+            for (u32 c = 0; c < count; c++)
+            {
+                u32 index = 0;
+                for (size_t i = 0; i < layerInfo.size(); i++)
+                {
+                    for (u32 o = 0; o < layerInfo[i]; o++)
+                    {
+                        correctInError[c*numInputDims + index] = outError[c*numOutputDims + i];
+                        index++;
+                    }
+                }
+                t.assert(index == numInputDims);
+                t.assert(layerInfo.size() == numOutputDims);
+            }
+
+            layer.takeOutputErrorGradients(outError, numOutputDims, count,
+                                           input, numInputDims, count,
+                                           true);
+
+            u32 retNumInputDims = 0, retCount = 0;
+            const fml* inError = layer.getInputErrorGradients(retNumInputDims, retCount);
+            t.assert(retNumInputDims == numInputDims);
+            t.assert(retCount == count);
+
+            for (u32 c = 0; c < count; c++)
+            {
+                for (u32 i = 0; i < numInputDims; i++)
+                {
+                    if (fabs(correctInError[c*numInputDims + i] - inError[c*numInputDims + i]) > 0.00001)
+                    {
+                        std::cerr << correctInError[c*numInputDims + i] << "  " << inError[c*numInputDims + i] << std::endl;
+                        t.fail();
+                    }
+                }
+            }
+        }
+
+        delete [] correctInError;
+        delete [] outError;
+        delete [] correctOutput;
+        delete [] input;
+    }
+}
+
+
 int main()
 {
     tCrashReporter::init();
 
     srand(time(0));
 
-    tTest("splitting layer test", test, kTestIterations);
+    tTest("splitting layer test (1)", test1, kTestIterations);
+    tTest("splitting layer test (2)", test2, kTestIterations);
 
     return 0;
 }
